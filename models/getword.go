4f86101e2f9c2e3cfe40943fd3e78a8fffb7173d
Joop Kiefte <joop@kiefte.net>
2014-10-20 11:59:45 +0200

    Lockless and modified-based json/terminaro system

--- models/getword.go
+++ models/getword.go
@@ -5,40 +5,55 @@ import (
 	"github.com/astaxie/beego"
 	"encoding/json"
 	"os"
-	"sync"
 	"time"
 	"strings"
 )
 
-type SafeTerminaro struct {
-	T *data.Terminaro
-	lastUpdated time.Time
-	sync.Mutex
-}
-
-var theTerminaro SafeTerminaro
+var theTerminaro data.Terminaro
+var t chan data.Terminaro
 var entryCache map[[2]string]data.Terminaro
 
-func initData() (terminaro *data.Terminaro, err error) {
-	var t data.Terminaro
-	terminaro = &t
-	file, err := os.Open(beego.AppConfig.String("terminaro"))
-	if err != nil { panic(err.Error() + ", probably json not correctly invoked, current terminaro conf entry is " + beego.AppConfig.String("terminaro")) }
-	err = json.NewDecoder(file).Decode(terminaro)
-	return
+func init() {
+	t = make(chan data.Terminaro,0)
+	terminaro := make(data.Terminaro,0)
+	lastModTime := time.Now()
+	go func() {
+		for {
+			file, err := os.Open(beego.AppConfig.String("terminaro"))
+			if err != nil {
+				beego.Error(err.Error() + ", probably json not correctly invoked, current terminaro conf entry is " + beego.AppConfig.String("terminaro"))
+			}
+			stat, err := file.Stat()
+			if err != nil {
+				beego.Error(err.Error())
+				continue
+			}
+			if stat.ModTime() != lastModTime {
+				err = json.NewDecoder(file).Decode(&terminaro)
+				if err == nil {
+					t <- terminaro
+					lastModTime = stat.ModTime()
+				} else {
+					beego.Error(err.Error())
+				}
+			}
+			time.Sleep(1 * time.Minute)
+		}
+	}()
 }
 
-func (t *SafeTerminaro) Get() *SafeTerminaro {
-	if time.Now().Unix() > t.lastUpdated.Add(10 * time.Minute).Unix() {
-		terminaro, err := initData()
-		if err != nil { panic(err.Error()) }
-		entryCache = nil
-		t.Lock()
-		t.T = terminaro
-		t.lastUpdated = time.Now()
-		t.Unlock()
+func GetTerminaro() (t2 data.Terminaro) {
+	if theTerminaro == nil {
+		theTerminaro = make(data.Terminaro,0)
 	}
-	return t
+	select {
+	case theTerminaro = <-t:
+		entryCache = nil
+	default:
+	}
+	t2 = make(data.Terminaro, len(theTerminaro))
+	copy(t2, theTerminaro)
+	return t2
 }
 
 func GetEntries(lang string, target string) (re data.Terminaro) {
@@ -46,24 +61,30 @@ func GetEntries(lang string, target string) (re data.Terminaro) {
 	if len(re) != 0 {
 		return re
 	}
-	theTerminaro.Get().Lock()
-	defer theTerminaro.Get().Unlock()
-	terminaro := *theTerminaro.Get().T
+	terminaro := GetTerminaro()
 	if entryCache == nil { entryCache = make(map[[2]string]data.Terminaro) }
-	for _, entry := range terminaro[1:] {
-		for _, translation := range entry.Translations {
-			if translation.Language == lang || lang == "" {
-				for _, word := range translation.Words {
-					if len(word.Written) < len(target) { continue }
-					if strings.ToLower(word.Written[:len(target)]) == strings.ToLower(target) {
-						re = append(re, entry)
+	if len(terminaro) > 1 {
+		for _, entry := range terminaro[1:] {
+			for _, translation := range entry.Translations {
+				if translation.Language == lang || lang == "" {
+					for _, word := range translation.Words {
+						if len(word.Written) < len(target) {
+							continue
+						}
+						if strings.ToLower(word.Written[:len(target)]) == strings.ToLower(target) {
+							re = append(re, entry)
+						}
 					}
 				}
 			}
 		}
-	}
-	re.SortBy(lang, target)
+	} else { beego.Error("Empty terminaro") }
+	re.SortLang(lang, target)
 	entryCache[[2]string{lang,target,}] = re
-	return
+	return re
+}
+
+func GetLangs() map[string]string {
+	return map[string]string{"en": "Angla", "eo": "Esperanto", "nl": "Nederlanda", "de": "Germana", "fr": "Franca",}
 }
 
